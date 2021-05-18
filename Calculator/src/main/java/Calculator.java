4c843c398cbe6402c5dd6d06f1548f93ffbcc026
gbethani <baranova_maria@rambler.ru>
2021-05-18 22:54:26 +0300

    test calculator

--- Calculator/src/main/java/Calculator.java
+++ Calculator/src/main/java/Calculator.java
@@ -7,45 +7,36 @@ public class Calculator {
         int a = scanner1.nextInt();
         System.out.println("Введите первое знак (+, -, /, *): ");
         Scanner scanner2 = new Scanner(System.in);
-        String z = scanner2.nextLine();
-        z = z.trim();
+        Character z = scanner2.next().charAt(0);
         System.out.println("Введите второе число: ");
         Scanner scanner3 = new Scanner(System.in);
         int b = scanner3.nextInt();
-        
-        switch (z)
-        {
-            case ("+"):
-                System.out.println(sum(a, b));
-                break;
-            case ("-"):
-                System.out.println(diff(a, b));
-                break;
-            case ("*"):
-                System.out.println(add(a, b));
-                break;
-            case ("/"):
-                System.out.println(division(a, b));
-                break;
-            default:
-                System.out.println("Введен неправильный знак!");
-        }
-    }
-    public static int sum(int a, int b)
-    {
-        return a+b;
-    }
-    public static int diff(int a, int b)
-    {
-        return a-b;
-    }
-    public static int add(int a, int b)
-    {
-        return a*b;
-    }
-    public static double division(int a, int b)
-    {
-        return (double)a/(double)b;
+        int result = calculator(a, z, b);
+        System.out.println("Результат: " + result);
     }
 
+    public static int calculator(int a, char z, int b) {
+        int result = 0;
+        switch (z) {
+            case ('+'):
+                result = a + b;
+                break;
+            case ('-'):
+                result = a - b;
+                break;
+            case ('*'):
+                result = a * b;
+                break;
+            case ('/'):
+                if (b == 0) {
+                    System.out.println("На ноль делить нельзя!");
+                    break;
+                }
+                result = a / b;
+                break;
+            default:
+                System.out.println("Неправильный ввод!");
+        }
+        return result;
+    }
 }
